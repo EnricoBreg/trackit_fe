5c23fbc552ad7a499be1257701a424aa040ef95f
Enrico Bregoli <bregolienrico@gmail.com>
2025-12-30 12:12:48 +0100

    Spostate configurazioni nel file App.tsx invece che mantenerle nel main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
 import "./App.css";
-import LoginForm from "./components/LoginForm";
+import { Provider } from "@/routes/-components/ui/provider";
+
+import "./i18n/i18n.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Import del routeTree automaticamente generato dal router
+import { routeTree } from "./routeTree.gen";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+
+// Creazione della nuova istanza del query client
+const queryClient = new QueryClient();
+// Creazione della nnuova istanza del router
+const router = createRouter({ routeTree, context: { queryClient } });
+
+// Register dell'instanza del router per avere type-safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+declare module "@tanstack/react-query" {
+  interface AppRouterContext {
+    queryClient: QueryClient;
+  }
+}
 
 function App() {
-  return <LoginForm />;
+  return (
+    <Provider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Provider>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,10 @@
-import { Provider } from "@/routes/-components/ui/provider";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./i18n/i18n.ts";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-// Import del routeTree automaticamente generato dal router
-import { routeTree } from "./routeTree.gen";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
-
-// Creazione della nnuova istanza del router
-const router = createRouter({ routeTree });
-// Creazione della nuova istanza del query client
-const queryClient = new QueryClient();
-
-// Register dell'instanza del router per avere type-safety
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
+import { StrictMode } from "react";
+import App from "@/App.tsx";
 
 // Render dell'applicazione
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </Provider>
+    <App />
   </StrictMode>,
 );
