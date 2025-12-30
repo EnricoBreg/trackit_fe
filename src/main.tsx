6dd5723f35ce8bc9a6b7aacc4e563289905a7e3c
Enrico Bregoli <bregolienrico@gmail.com>
2025-12-30 11:26:43 +0100

    Query: aggiunta al progetto libreria tanstack Query

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { Provider } from "@/routes/-components/ui/provider";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./i18n/i18n.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // Import del routeTree automaticamente generato dal router
 import { routeTree } from "./routeTree.gen";
@@ -9,6 +11,8 @@ import { createRouter, RouterProvider } from "@tanstack/react-router";
 
 // Creazione della nnuova istanza del router
 const router = createRouter({ routeTree });
+// Creazione della nuova istanza del query client
+const queryClient = new QueryClient();
 
 // Register dell'instanza del router per avere type-safety
 declare module "@tanstack/react-router" {
@@ -21,7 +25,10 @@ declare module "@tanstack/react-router" {
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 );
