1281b31cc7442280005c6633777ff50f5eeb7ef3
Enrico Bregoli <bregolienrico@gmail.com>
2026-01-07 21:42:04 +0100

    Ulteriore avanzamento con pagina dettaglio utente

--- src/routes/app/users/$userId.tsx
+++ src/routes/app/users/$userId.tsx
@@ -1,6 +1,18 @@
+import Clipboard from "@/components/Clipboard";
 import useUser from "@/hooks/useUser";
 import userQueryOptions from "@/queries/userQuery";
-import { Spinner } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  HStack,
+  Spinner,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { createFileRoute, useParams } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/app/users/$userId")({
@@ -19,8 +31,60 @@ function RouteComponent() {
   if (error || !user) throw error;
 
   return (
-    <div>
-      Hello "/app/users/{userId}"! {user.nominativo}
-    </div>
+    <Grid templateColumns={"repeat(3, 1fr)"} gap={4}>
+      <GridItem>
+        <Flex justifyContent="center" alignItems="center">
+          <Avatar.Root size={"2xl"} variant={"solid"}>
+            <Avatar.Fallback name={user.nome + " " + user.cognome} />
+            <Avatar.Image />
+          </Avatar.Root>
+        </Flex>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <VStack spaceY={4} align="start">
+          <Box>
+            <Heading
+              as="h1"
+              fontSize={{ base: "2xl", md: "4xl" }}
+              fontWeight="bold"
+            >
+              {user.nome + " " + user.cognome}
+            </Heading>
+            <Text fontSize="xl" color="gray.400">
+              @{user.username}
+            </Text>
+          </Box>
+
+          <Box>
+            <Heading
+              as="h3"
+              fontSize={{ base: "lg", md: "xl" }}
+              fontWeight="bold"
+            >
+              Contatti
+            </Heading>
+
+            <VStack>
+              {/* Elemento lista contatti */}
+              <Flex gap={10} alignItems="center">
+                <Box>Email</Box>
+                <HStack>
+                  {user.email}
+                  <Clipboard value={user.email} size="xs" />
+                </HStack>
+              </Flex>
+
+              <Flex gap={10} alignItems="center">
+                <Box>Telefono</Box>
+                <HStack>
+                  00393338087889
+                  <Clipboard value="00393338087889" size="xs" />
+                </HStack>
+              </Flex>
+            </VStack>
+          </Box>
+        </VStack>
+      </GridItem>
+    </Grid>
   );
 }
