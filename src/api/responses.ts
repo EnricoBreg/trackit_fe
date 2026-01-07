af613b7d7eb492b9c7dabf922854774f510e209e
Enrico Bregoli <bregolienrico@gmail.com>
2026-01-07 18:07:56 +0100

    Cambiata FetchResponse per contestuale cambiamento nel backend

--- src/api/responses.ts
+++ src/api/responses.ts
@@ -1,11 +1,16 @@
-/*
-export interface FetchResponse<T> {
-  results: T[];
-} */
-
 import type User from "@/entities/User";
 
-export type FetchResponse<T> = T[];
+/* export type FetchResponse<T> = T[]; */
+
+export interface FetchResponse<T> {
+  results: T[];
+  currentPage: number;
+  totalPage: number;
+  pageSize: number;
+  hasNext: boolean;
+  hasPrevious: boolean;
+}
+
 export interface LoginResponse {
   accessToken: string;
   user: User;
