10ffc6e47693437dbb1627a3a5b69f54162a6ed6
Enrico Bregoli <bregolienrico@gmail.com>
2026-01-12 23:00:31 +0100

    Aggiunto clear button al componente searchInput

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,6 +1,6 @@
 import useAppTranslation from "@/hooks/useTranslation";
-import { Input, InputGroup } from "@chakra-ui/react";
-import { useRef, type FormEvent } from "react";
+import { CloseButton, Input, InputGroup } from "@chakra-ui/react";
+import { useRef, useState, type FormEvent } from "react";
 import { LuSearch } from "react-icons/lu";
 
 interface SearchInputProps {
@@ -10,21 +10,38 @@ interface SearchInputProps {
 
 const SearchInput = ({ searchText, setSearchTextFn }: SearchInputProps) => {
   const { t } = useAppTranslation();
-  const ref = useRef<HTMLInputElement>(null);
+  const ref = useRef<HTMLInputElement | null>(null);
+
+  const [value, setValue] = useState(searchText);
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     if (ref.current) setSearchTextFn(ref.current.value);
   };
 
+  const endElement = value ? (
+    <CloseButton
+      size="xs"
+      onClick={() => {
+        setValue("");
+        setSearchTextFn("");
+        ref.current?.focus();
+      }}
+      me="-2"
+    />
+  ) : undefined;
+
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
-      <InputGroup flex="1" startElement={<LuSearch />}>
+      <InputGroup flex="1" startElement={<LuSearch />} endElement={endElement}>
         <Input
           placeholder={t("ricercaPlaceholder")}
           ref={ref}
           borderRadius={5}
-          defaultValue={searchText}
+          value={value}
+          onChange={(e) => {
+            setValue(e.currentTarget.value);
+          }}
         />
       </InputGroup>
     </form>
