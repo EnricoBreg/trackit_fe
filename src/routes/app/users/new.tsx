985434505951909b048b990c90718c54b88f8175
Enrico Bregoli <bregolienrico@gmail.com>
2026-01-18 19:15:49 +0100

    Users: gestito caso di errore nella richiesta di creazione nuovo utente

--- src/routes/app/users/new.tsx
+++ src/routes/app/users/new.tsx
@@ -1,16 +1,28 @@
+import {
+  handleBackendErrors,
+  type BackendErrorResponse,
+} from "@/api/responses";
 import GenericHeading from "@/components/GenericHeading";
 import { PasswordInput } from "@/components/ui/password-input";
-import Wizard from "@/components/Wizard";
+import Wizard, { type WizardStep } from "@/components/Wizard";
 import type User from "@/domain/entities/User";
 import { createUserSchema } from "@/domain/features/users/user-wizard.schema";
 import type { CreateUserForm } from "@/domain/features/users/user-wizard.types";
 import useAppTranslation from "@/hooks/useTranslation";
 import ApiClient from "@/services/api-client";
-import { Box, Field, GridItem, Input, SimpleGrid } from "@chakra-ui/react";
+import {
+  Box,
+  Field,
+  GridItem,
+  Input,
+  SimpleGrid,
+  useSteps,
+} from "@chakra-ui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
-import { useFormContext } from "react-hook-form";
+import type { AxiosError } from "axios";
+import { useFormContext, type UseFormReturn } from "react-hook-form";
 
 export const Route = createFileRoute("/app/users/new")({
   component: RouteComponent,
@@ -22,15 +34,42 @@ function RouteComponent() {
   const { t } = useAppTranslation();
   const router = useRouter();
 
-  const { mutate, isPending } = useMutation({
+  const { mutateAsync, isPending } = useMutation({
     mutationFn: apiClient.post,
-
-    onSuccess: () => router.navigate({ to: "/app/users" }),
-
-    onError: (err) => console.error("Error", err),
   });
 
-  const onSubmit = (formValues: CreateUserForm) => mutate(formValues);
+  const steps: WizardStep<CreateUserForm>[] = [
+    {
+      title: t("utenti.infoUtenteStep"),
+      component: UserInfoStep,
+      validationFields: ["nome", "cognome", "username", "email"],
+    },
+    {
+      title: t("utenti.passwordStep"),
+      component: UserPasswordStep,
+      validationFields: ["password", "confirmPassword"],
+    },
+  ];
+
+  const onSubmit = async (
+    formData: CreateUserForm,
+    helpers: {
+      methods: UseFormReturn<CreateUserForm>;
+      stepsApi: ReturnType<typeof useSteps>;
+    },
+  ) => {
+    try {
+      await mutateAsync(formData);
+      router.navigate({ to: "/app/users" });
+    } catch (error) {
+      handleBackendErrors(
+        error as AxiosError<BackendErrorResponse>,
+        helpers.methods,
+        steps,
+        helpers.stepsApi,
+      );
+    }
+  };
 
   return (
     <Box spaceY={4}>
@@ -38,18 +77,7 @@ function RouteComponent() {
 
       <div>
         <Wizard<CreateUserForm>
-          steps={[
-            {
-              title: t("utenti.infoUtenteStep"),
-              component: UserInfoStep,
-              validationFields: ["nome", "cognome", "username", "email"],
-            },
-            {
-              title: t("utenti.passwordStep"),
-              component: UserPasswordStep,
-              validationFields: ["password", "confirmPassword"],
-            },
-          ]}
+          steps={steps}
           resolver={zodResolver(createUserSchema)}
           onSubmit={onSubmit}
           completedContentText={t("utenti.confermaCreazione")}
