4d257a66ca24a36952d3ab40ee6918cb9dd39fb2
Enrico Bregoli <bregolienrico@gmail.com>
2026-02-10 23:01:08 +0100

    Miglioria all'utility di formattazione delle date

--- src/utils/formatDateByLocale.ts
+++ src/utils/formatDateByLocale.ts
@@ -1,8 +1,15 @@
-const formatDateByLocale = (date: Date, locale: string) => {
+const formatDateByLocale = (date: Date | string, locale: string) => {
+  let d: Date;
+  if (date instanceof Date) {
+    d = date;
+  } else {
+    d = new Date(date);
+  }
+
   return new Intl.DateTimeFormat(locale, {
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
-  }).format(date);
+  }).format(d);
 };
 export default formatDateByLocale;
